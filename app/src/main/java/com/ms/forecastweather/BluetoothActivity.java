b03fc4df19f09bc3dd7655e12f28ab5d174c026d
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-04-18 12:48:11 -0400

    Explore Bluetooth devices screen was updated.

--- app/src/main/java/com/ms/forecastweather/BluetoothActivity.java
+++ app/src/main/java/com/ms/forecastweather/BluetoothActivity.java
@@ -13,7 +13,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,9 +28,10 @@ public class BluetoothActivity extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 0;
     private static final int REQUEST_DISCOVER_BT = 1;
 
-    TextView mStatusBlueTv, mPairedTv;
+    TextView mStatusBlueTv, mPairedTv, bluetoothStatusText;
     ImageView mBlueTv;
     Button mOnBtn, mOffBtn, mDiscoveredBtn, mPairedBtn;
+    Switch bluetoothStatus;
 
     BluetoothAdapter mBlueAdapter;
 
@@ -37,6 +40,11 @@ public class BluetoothActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
 
+        // Initialized Bluetooth Status switch
+        bluetoothStatus = findViewById(R.id.switchBluetooth);
+
+        // Bluetooth Status text view
+        bluetoothStatusText = findViewById(R.id.bluetoothStatus);
 
         // Initialize the assign value
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -44,7 +52,7 @@ public class BluetoothActivity extends AppCompatActivity {
         // Set Home Selected
         bottomNavigationView.setSelectedItemId(R.id.explore);
 
-        // Perform ItemselectionListener
+        // Perform Item selectionListener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -64,72 +72,102 @@ public class BluetoothActivity extends AppCompatActivity {
             }
         });
 
+        // Change Bluetooth On/Off Status
+        bluetoothStatus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b == true) {
+                    if (!mBlueAdapter.isEnabled()){
+                        showToast("Turing On Bluetooth...");
+                        // intent to on Bluetooth
+                        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(intent, REQUEST_ENABLE_BT);
+                        bluetoothStatusText.setText("Bluetooth is One");
+                    }
+                    else {
+                        showToast("Bluetooth is already on");
+                        bluetoothStatusText.setText("Bluetooth is One");
+                    }
+                } else {
+                    if (mBlueAdapter.isEnabled()){
+                        mBlueAdapter.disable();
+                        showToast("Turning Bluetooth Off");
+                        bluetoothStatusText.setText("Bluetooth is Off");
+//                    mBlueTv.setImageResource(R.drawable.ic_action_off);
+                    } else {
+                        showToast("Bluetooth is already off");
+                        bluetoothStatusText.setText("Bluetooth is Off");
+                    }
+                }
+            }
+        });
 
-        mStatusBlueTv = findViewById(R.id.statusBluetoothTV);
+
+//        mStatusBlueTv = findViewById(R.id.statusBluetoothTV);
         mPairedTv = findViewById(R.id.pairedTv);
-        mBlueTv = findViewById(R.id.bluetoothTV);
-        mOnBtn = findViewById(R.id.onBtn);
-        mOffBtn = findViewById(R.id.offBtn);
-        mDiscoveredBtn = findViewById(R.id.discoverableBtn);
+//        mBlueTv = findViewById(R.id.bluetoothTV);
+//        mOnBtn = findViewById(R.id.onBtn);
+//        mOffBtn = findViewById(R.id.offBtn);
+//        mDiscoveredBtn = findViewById(R.id.discoverableBtn);
         mPairedBtn = findViewById(R.id.pairedBtn);
 
         // adapter
         mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        // check if bluetooth is available
-        if (mBlueAdapter == null){
-            mStatusBlueTv.setText("Bluetooth is not available");
-        } else {
-            mStatusBlueTv.setText("Bluetooth is available");
-        }
+//        // check if bluetooth is available
+//        if (mBlueAdapter == null){
+//            mStatusBlueTv.setText("Bluetooth is not available");
+//        } else {
+//            mStatusBlueTv.setText("Bluetooth is available");
+//        }
 
         // set image according to the bluetooth status(on/off)
-        if (mBlueAdapter.isEnabled()){
-            mBlueTv.setImageResource(R.drawable.ic_action_on);
-        }
-        else {
-            mBlueTv.setImageResource(R.drawable.ic_action_off);
-        }
+//        if (mBlueAdapter.isEnabled()){
+//            mBlueTv.setImageResource(R.drawable.ic_action_on);
+//        }
+//        else {
+//            mBlueTv.setImageResource(R.drawable.ic_action_off);
+//        }
 
-        // on btn click
-        mOnBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!mBlueAdapter.isEnabled()){
-                     showToast("Turing On Bluetooth...");
-                     // intent to on Bluetooth
-                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(intent, REQUEST_ENABLE_BT);
-                }
-                else {
-                    showToast("Bluetooth is already on");
-                }
-            }
-        });
-        // Discover bluetooth btn
-        mDiscoveredBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!mBlueAdapter.isDiscovering()){
-                    showToast("Making your device discoverable");
-                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                    startActivityForResult(intent, REQUEST_ENABLE_BT);
-                }
-            }
-        });
-        // off btn click
-        mOffBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mBlueAdapter.isEnabled()){
-                    mBlueAdapter.disable();
-                    showToast("Turning Bluetooth Off");
-                    mBlueTv.setImageResource(R.drawable.ic_action_off);
-                } else {
-                    showToast("Bluetooth is already off");
-                }
-            }
-        });
+//        // on btn click
+//        mOnBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (!mBlueAdapter.isEnabled()){
+//                     showToast("Turing On Bluetooth...");
+//                     // intent to on Bluetooth
+//                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//                    startActivityForResult(intent, REQUEST_ENABLE_BT);
+//                }
+//                else {
+//                    showToast("Bluetooth is already on");
+//                }
+//            }
+//        });
+//        // Discover bluetooth btn
+//        mDiscoveredBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (!mBlueAdapter.isDiscovering()){
+//                    showToast("Making your device discoverable");
+//                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+//                    startActivityForResult(intent, REQUEST_ENABLE_BT);
+//                }
+//            }
+//        });
+//        // off btn click
+//        mOffBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (mBlueAdapter.isEnabled()){
+//                    mBlueAdapter.disable();
+//                    showToast("Turning Bluetooth Off");
+////                    mBlueTv.setImageResource(R.drawable.ic_action_off);
+//                } else {
+//                    showToast("Bluetooth is already off");
+//                }
+//            }
+//        });
         // get paired devices btn click
         mPairedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -157,7 +195,7 @@ public class BluetoothActivity extends AppCompatActivity {
             case REQUEST_ENABLE_BT:
                 if (resultCode == RESULT_OK){
                     // bluetooth is one
-                    mBlueTv.setImageResource(R.drawable.ic_action_on);
+//                    mBlueTv.setImageResource(R.drawable.ic_action_on);
                     showToast("Bluetooth is on");
                 } else {
                     // under denied to turn bluetooth on
