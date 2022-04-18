a7d5d1c603134299835aba173f694c6317b78e22
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-04-18 17:03:38 -0400

    Business Details screen was implemented.

--- app/src/main/java/com/ms/forecastweather/BusinessDetailsActivity.java
+++ app/src/main/java/com/ms/forecastweather/BusinessDetailsActivity.java
@@ -2,13 +2,23 @@ package com.ms.forecastweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class BusinessDetailsActivity extends AppCompatActivity {
+
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_details);
+
+        Bundle values = getIntent().getExtras(); // Need a reference to intent
+        url = values.getString("url");
+
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(browserIntent);
     }
 }
